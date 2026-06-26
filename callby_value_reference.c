787477a2a9a9ae8f09c226e161a31179c5eed513
gitpoojagautam <bharatgautam6907@gmail.com>
2026-06-26 06:47:32 -0700

    Create callby_value_reference.c
    call by value vs call by reference demonstration

---
+++ callby_value_reference.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void value(int a)
+{
+    a = 100;
+}
+
+void reference(int *a)
+{
+    *a = 100;
+}
+
+int main()
+{
+    int x = 10;
+
+    value(x);
+    printf("After Call by Value = %d\n", x);
+
+    reference(&x);
+    printf("After Call by Reference = %d", x);
+
+    return 0;
+}
