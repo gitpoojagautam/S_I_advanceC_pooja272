748b2d6819a47357609486d3cc2fd7f4751e353e
gitpoojagautam <bharatgautam6907@gmail.com>
2026-06-26 06:51:05 -0700

    compare_strings.c
    compare two strings without strcmp

---
+++ compare_strings.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char str1[] = "Hello";
+    char str2[] = "Hello";
+    int i = 0;
+
+    while(str1[i] == str2[i] && str1[i] != '\0')
+        i++;
+
+    if(str1[i] == str2[i])
+        printf("Strings are Equal");
+    else
+        printf("Strings are Not Equal");
+
+    return 0;
+}
