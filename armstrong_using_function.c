e685e10f5170df7b03d69f665594be692a33055e
gitpoojagautam <bharatgautam6907@gmail.com>
2026-06-26 06:31:51 -0700

    armstrong_using_function.c
    armstrong numbers using function

---
+++ armstrong_using_function.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int armstrong(int n)
+{
+    int sum = 0, rem, temp = n;
+
+    while(temp > 0)
+    {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    return sum == n;
+}
+
+int main()
+{
+    int n = 153;
+
+    if(armstrong(n))
+        printf("Armstrong");
+    else
+        printf("Not Armstrong");
+
+    return 0;
+}
